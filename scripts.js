4ca6bcd42b9bc8d517219e88ca9af75c33af5a29
Fahime kolahi <farabicteach@gmail.com>
2024-03-05 16:35:39 +0330

    add post delete

--- scripts.js
+++ scripts.js
@@ -8,8 +8,8 @@ let number = document.getElementById("number")
 let email = document.getElementById("email")
 let lastName = document.getElementById("lastName")
 let name = document.getElementById("name")
-let userInp= document.getElementById("userInp")
-let usersList=document.getElementById("usersList")
+let userInp = document.getElementById("userInp")
+let usersList = document.getElementById("usersList")
 
 function openModal() {
     modal.style.display = "block";
@@ -25,89 +25,161 @@ function closeModal() {
     kol.style.display = "none";
 }
 
-let users = [];
-function addNewUser(){
+
+function addNewUser() {
 
 
-        let person = {
-            thename: name.value,
-            thelastName: lastName.value,
-            theemail: email.value,
-            thenumber: number.value,
+    let person = {
+        name: name.value,
+        family: lastName.value,
+        email: email.value,
+        phoneNumber: number.value,
 
+    }
+    // console.log(person)
+    // users.push(person);
+    // render()
+    // console.log(users)
+
+
+    fetch('https://602bf8bf30ba7200172227a8.mockapi.io/users', {
+        method: "post",
+        body: JSON.stringify(person),
+        headers:{
+            "Content-Type" : "application/json"
         }
-users.push(person);
-render()
-console.log(users)
+
+
+    })
+
+.then(res=>{
+    render()
+})
+.catch(err=>{
+    console.log(err)
+})
+
 }
 
 
 
 
 
-function deleteUser(userId){
+// function deleteUser(userId) {
 
-users = users.filter(item=> item.id !== userId)
+//     users = users.filter(item => item.id !== userId)
 
 
-render()
+//     render()
+// }
+
+function editUser(id) {
+    fetch(`https://602bf8bf30ba7200172227a8.mockapi.io/users/${id}`,{
+        method : "PUT",
+        body: JSON.stringify({
+            name: "",
+            lastName: "",
+            email: "",
+            number: ""
+        }),
+        headers : {
+            "Content-Type" : "application/json"
+        }
+    }).then(res=>{
+        render()
+    }).catch(err=>console.log(err))
 }
 
 
 
 
-
-
- ///dom
+///dom
 
 function addBtn() {
-addNewUser()
+    addNewUser()
 
-name.value=""
-lastName.value=""
-email.value=""
-number.value=""
+    name.value = ""
+    lastName.value = ""
+    email.value = ""
+    number.value = ""
 
 }
 
 
 
 
-function deleteBtn(){
+function deleteBtn(id) {
 
-deleteUser(userId)
+    fetch(`https://602bf8bf30ba7200172227a8.mockapi.io/users/${id}`,{
+        method: "delete"
+    }).then(res=>{
+        render()
+    }).catch(err=>console.log(err))
+
+
+
+    
 }
+
+
+
+function editBtn(id) {
+editUser()
+    editUserStatus = true
+    editUserId = id
+    changeBtn()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 let erroList = {
-nameError: "باید بیشتر از سه حرف باشد"
+    nameError: "باید بیشتر از سه حرف باشد"
 
 
 }
 
 
 
-function render(){
-
+function render() {
     let temp = ""
+    
+fetch('https://602bf8bf30ba7200172227a8.mockapi.io/users')
+.then(response=>response.json())
+.then(result=>{
+    result.forEach(item=>{
+        
+        temp += `
 
-users.forEach(item => {
-temp += `
-<div class="">
 
-        <div class="flex  bg-[#F3F4F6] w-[60%] mx-auto flex justify-around py-[1rem] rounded-[0.8rem] font-bold">
+        <div class="flex sm:text-[15px] sm:w-[60%] w-[70%] text-[10px] rounded-[0.8rem] justify-around mx-auto gap-1 my-[1rem]">
 
             <div>
                 <p>${item.name}</p>
             </div>
 
             <div>
-                <p>${item.lastName}</p>
+                <p>${item.family}</p>
             </div>
 
             <div>
-                <p>${item.number}</p>
+                <p>${item.phoneNumber}</p>
             </div>
 
             <div>
@@ -118,9 +190,9 @@ temp += `
 
             <div class="flex gap-2">
 
-                <div class="w-[32px] h-[32px] bg-[#C31C62] flex items-center justify-center rounded-[4px]">
+                <div class="w-[22px] h-[22px] sm:w-[32px] sm:h-[32px] bg-[#C31C62] flex items-center justify-center rounded-[4px]" onclick="editBtn(id)">
                     <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="white"
-                        class="bi bi-pencil-square" viewBox="0 0 16 16">
+                    class="w-[14px] h-[14px]" viewBox="0 0 16 16">
                         <path
                             d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                         <path fill-rule="evenodd"
@@ -129,9 +201,9 @@ temp += `
                 </div>
 
 
-                <div class="w-[32px] h-[32px] bg-[#C31C62] flex items-center justify-center rounded-[4px]" onclick="deleteBtn()">
+                <div class="w-[22px] h-[22px] sm:w-[32px] sm:h-[32px] bg-[#C31C62] flex items-center justify-center rounded-[4px]" onclick="deleteBtn(id)">
 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="white" class="bi bi-trash3"
+                    <svg xmlns="http://www.w3.org/2000/svg"  fill="white" class="w-[14px] h-[14px]"
                         viewBox="0 0 16 16">
                         <path
                             d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
@@ -157,22 +229,103 @@ temp += `
 
 
 
-    </div>
-
-
-
-
-
-
-
-
-
+    
 
 `
+usersList.innerHTML = temp
+    })
+.catch(err=>console.log(err))
 
+    usersList.innerHTML = temp
+
+})
+}
+
+    
+    
+       
+
+
+function changeBtn() {
+
+    if (editUserStatus) {
+
+        userInp.innerHTML = "ویرایش"
+
+
+    } else {
+        userInp.innerHTML = "افزودن"
+    }
+
+
+
+}
+
+///////////////////////////////////////////////////////////////// سوال بپرسم
+userInp.addEventListener('click', () => {
+    if (editUserStatus) {
+        console.log("edit handled")
+        editUser(editUserId,)
+    } else {
+        console.log("add handled")
+        addBtn()
+
+    }
 })
 
 
-usersList.innerHTML= temp
+changeBtn()
 
-}
+
+
+
+
+
+
+
+
+
+
+
+// const MAIN_URL = "https://602bf8bf30ba7200172227a8.mockapi.io/users"
+
+// //Get All Users
+// fetch(MAIN_URL)
+//     .then(res => res.json())
+//     .then(result => console.log(result))
+
+// // Post New User
+// fetch(MAIN_URL, {
+//     method: "POST",
+//     body: JSON.stringify({
+//         name: "",
+//         lastName: "",
+//         email: "",
+//         number: ""
+//     }),
+//     headers : {
+//         "Content-Type" : "application/json"
+//     }
+// })
+
+// // //Delete User
+// fetch(MAIN_URL + `/1` , {
+//     method: "DELETE",
+//     headers : {
+//         "Content-Type" : "application/json"
+//     }
+// })
+
+// // //Update User
+fetch(MAIN_URL + `/2`,{
+    method : "PUT",
+    body: JSON.stringify({
+        name: "",
+        lastName: "",
+        email: "",
+        number: ""
+    }),
+    headers : {
+        "Content-Type" : "application/json"
+    }
+})
